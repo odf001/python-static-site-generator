cea521bea1d2ce9628cfd21d9e85d812eb7f2490
odf001 <ofelicia@amfam.com>
2020-09-23 11:23:37 -0500

    Add code for Site Class
    Add code for site.py and ssg.py

---
+++ ssg.py
@@ -0,0 +1,11 @@
+import typer
+
+from ssg.site import site
+
+
+def main(source="content", dest='dist'):
+    config = {"source": source, "dest": dest}
+
+    Site(**config).build()
+
+typer.run(main)
